c16a2e75993382673257d7fd005fec9bb5e2cbc7
shanthi5080 <shanthi5080@yahoo.com>
2015-09-14 10:02:06 -0700

    Create MissingNumberInArray.java
    Finding a missing a number in array

---
+++ SampleProject/src/com/main/MissingNumberInArray.java
@@ -0,0 +1,36 @@
+package Loops;
+
+public class MissingNumberInArray {
+	
+	
+	
+
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			int[] number = { 10,20,30,35,45,60};
+
+			int numToFind =10 ,i = 0; 
+
+			while(i<number.length){
+				if(numToFind==number[i]){
+					System.out.println("Missing Number In Array is " + number[i] + " Index location is " + i);
+					i++;
+					break;
+
+				}
+				else{
+
+					i++;
+					continue;
+
+				}
+
+
+			}
+
+		}
+
+	}
